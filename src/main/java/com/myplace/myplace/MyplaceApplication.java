1314b9cc43855a7ab1b81eeed387a6c87b84a69d
skwwnl <skwwnl@gmail.com>
2023-03-04 16:25:15 +0900

    Feat : #30 리뷰 Entity 생성

--- src/main/java/com/myplace/myplace/MyplaceApplication.java
+++ src/main/java/com/myplace/myplace/MyplaceApplication.java
@@ -2,7 +2,9 @@ package com.myplace.myplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MyplaceApplication {
 
