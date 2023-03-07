17011b1fc67680c0b72af5e8e15d04e33effeba8
GEUNHOKIM <rmszkf@naver.com>
2023-03-07 12:46:05 +0900

    Chore : 기타 수정

--- src/main/java/com/myplace/myplace/review/dto/ReviewRequestDto.java
+++ src/main/java/com/myplace/myplace/review/dto/ReviewRequestDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Setter
 public class ReviewRequestDto {
 
-    private List<String> keywordList = new ArrayList<>();
+    private List<String> reviewKeywordList;
     private MultipartFile reviewPhotoUrl;
     private String reviewContents;
 
--- src/main/java/com/myplace/myplace/s3/service/S3Uploader.java
+++ src/main/java/com/myplace/myplace/s3/service/S3Uploader.java
@@ -4,10 +4,13 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -21,16 +24,28 @@ public class S3Uploader {
 
     public String upload(MultipartFile file) throws IOException {
 
-        String fileName = file.getOriginalFilename();
+        String fileName = createFileName(file.getOriginalFilename());
         String fileUrl = "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" + fileName;
 
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(file.getContentType());
         metadata.setContentLength(file.getSize());
 
-        amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
+        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
 
         return fileUrl;
+    }
+
+    private String createFileName(String fileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
+        }
     }
 
 }
