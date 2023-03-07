941558a26623544210e88f3ab2085658df1dab00
GEUNHOKIM <rmszkf@naver.com>
2023-03-07 12:42:02 +0900

    #10 Feat : 리뷰 조회 API 기능 구현

--- src/main/java/com/myplace/myplace/review/service/ReviewService.java
+++ src/main/java/com/myplace/myplace/review/service/ReviewService.java
@@ -9,9 +9,7 @@ import com.myplace.myplace.member.entity.Member;
 import com.myplace.myplace.member.repository.MemberRepository;
 import com.myplace.myplace.restaurant.entity.Restaurant;
 import com.myplace.myplace.restaurant.repository.RestaurantRepository;
-import com.myplace.myplace.review.dto.ReviewRequestDto;
-import com.myplace.myplace.review.dto.ReviewResponseDto;
-import com.myplace.myplace.review.dto.ReviewUpdateDto;
+import com.myplace.myplace.review.dto.*;
 import com.myplace.myplace.review.entity.Keyword;
 import com.myplace.myplace.review.entity.KeywordType;
 import com.myplace.myplace.review.entity.Review;
@@ -23,11 +21,10 @@ import com.myplace.myplace.s3.service.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.time.LocalDateTime;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -43,7 +40,6 @@ public class ReviewService {
     private final S3Uploader s3Uploader;
 
 
-    // 리뷰 작성
     @Transactional
     public SuccessResponseDto<Void> createReview(Long id, ReviewRequestDto requestDto, Member member) throws IOException {
 
@@ -53,14 +49,11 @@ public class ReviewService {
 
         String imgUrl = s3Uploader.upload(requestDto.getReviewPhotoUrl());
 
-        member.update(imgUrl);
-        memberRepository.save(member);
-
         Review review = Review.of(requestDto, imgUrl, member, restaurant);
 
         List<ReviewKeyword> reviewKeywordList = new ArrayList<>();
 
-        for (String keywordStr : requestDto.getKeywordList()) {
+        for (String keywordStr : requestDto.getReviewKeywordList()) {
 
             KeywordType type = KeywordType.valueOf(keywordStr);
             Keyword keyword = Keyword.of(type);
@@ -78,27 +71,13 @@ public class ReviewService {
         return ResponseUtils.ok(MessageType.REVIEW_WRITE_SUCCESSFULLY);
     }
 
-
-    // [피드, 리뷰] 상세 조회
-    @Transactional(readOnly = true)
-    public SuccessResponseDto<ReviewResponseDto> reviewDetail(Long id) {
-
+    @Transactional
+    public SuccessResponseDto<Void> updateReview(Long id, ReviewUpdateDto requestDto, Member member) {
 
         Review review = reviewRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException(ErrorType.NOT_FOUND_REVIEW.getMessage())
         );
 
-        int likeCount = likeRepository.countById(id);
-        int reviewCount = reviewRepository.countById(id);
-
-        ReviewResponseDto reviewResponseDto = ReviewResponseDto.of(review, likeCount, reviewCount);
-
-        return ResponseUtils.ok(reviewResponseDto, MessageType.REVIEW_INQUIRY_SUCCESSFULLY);
-   }
-
-
-    @Transactional
-    public SuccessResponseDto<Void> updateReview(Long id, ReviewUpdateDto requestDto, Member member) {
 
         if(!review.getMember().getMemberId().equals(member.getMemberId())) {
             throw new IllegalArgumentException(ErrorType.NOT_WRITER.getMessage());
@@ -115,6 +94,7 @@ public class ReviewService {
         Review review = reviewRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException(ErrorType.NOT_FOUND_REVIEW.getMessage())
         );
+
         if(!review.getMember().getMemberId().equals(member.getMemberId())){
             throw new IllegalArgumentException(ErrorType.NOT_WRITER.getMessage());
         }
@@ -123,4 +103,71 @@ public class ReviewService {
 
         return ResponseUtils.ok(MessageType.REVIEW_DELETE_SUCCESSFULLY);
     }
+
+    // [피드, 리뷰] 상세 조회
+
+    @Transactional(readOnly = true)
+    public SuccessResponseDto<ReviewDetailDto> reviewDetail(Long id) {
+
+
+        Review review = reviewRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException(ErrorType.NOT_FOUND_REVIEW.getMessage())
+        );
+
+        List<String> reviewKeywordList = new ArrayList<>();
+
+        for(ReviewKeyword r : review.getReviewKeywordList()) {
+            String keywordList = r.getKeyword().getType().getMsg();
+
+            reviewKeywordList.add(keywordList);
+        }
+
+        int likeCount = likeRepository.findByReviewId(id).size();
+        int reviewCount = reviewRepository.findByMember_Id(review.getMember().getId()).size();
+
+        ReviewDetailDto reviewDetailDto = ReviewDetailDto.of(review, likeCount, reviewCount, reviewKeywordList);
+
+        return ResponseUtils.ok(reviewDetailDto, MessageType.REVIEW_INQUIRY_SUCCESSFULLY);
+    }
+
+//    // 리뷰 조회
+    @Transactional(readOnly = true)
+    public SuccessResponseDto<List<ReviewResponseDto>> myreviews(Member member) {
+
+        List<Review> reviewList = reviewRepository.findByMember_Id(member.getId());
+
+        List<ReviewResponseDto> reviewResponseDtoList = new ArrayList<>();
+
+        int reviewCount = reviewRepository.findByMember_Id(member.getId()).size();
+
+        for(Review r : reviewList) {
+
+            ReviewResponseDto reviewResponseDto = ReviewResponseDto.of(r, reviewCount);
+
+            reviewResponseDtoList.add(reviewResponseDto);
+        }
+
+        return ResponseUtils.ok(reviewResponseDtoList, MessageType.REVIEW_INQUIRY_SUCCESSFULLY);
+    }
+
+//    // 피드 조회
+//    @Transactional(readOnly = true)
+//    public SuccessResponseDto<List<FeedReviewResponseDto>> feedReviews() {
+//
+//        List<Review> reviewList = reviewRepository.findAll();
+//
+//        List<FeedReviewResponseDto> feedreviewResponseDtoList = new ArrayList<>();
+//
+//        for(Review r : reviewList) {
+//
+//            int likeCount = likeRepository.findByReviewId(r.getId()).size();
+//            int reviewCount = reviewRepository.findByMember_Id(r.getMember().getId()).size();
+//
+//            FeedReviewResponseDto feedreviewResponseDto = FeedReviewResponseDto.of(r, likeCount, reviewCount, );
+//
+//            feedreviewResponseDtoList.add(feedreviewResponseDto);
+//        }
+//
+//        return ResponseUtils.ok(feedreviewResponseDtoList, MessageType.REVIEW_INQUIRY_SUCCESSFULLY);
+//    }
 }
