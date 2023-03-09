ad6ec8c3f63c9b8e3a718d9990c7a62fa2264ee7
doyeonKim <0915dy@gmail.com>
2023-03-09 12:20:00 +0900

    Feat: #7 피드 전체 조회 시 사용자 프로필 이미지 리턴
    - 사용자 프로필 이미지 url 리턴을 위해 `FeedPageResponseDto`에 `loginProfileImgUrl` 필드 추가
    - 로그인한 경우, 로그인한 사용자의 프로필 이미지 Url을 리턴
    - 로그인하지 않은 경우, default 이미지 url 을 리턴

--- src/main/java/com/myplace/myplace/review/dto/FeedPageResponseDto.java
+++ src/main/java/com/myplace/myplace/review/dto/FeedPageResponseDto.java
@@ -9,17 +9,20 @@ import java.util.List;
 public class FeedPageResponseDto {
 
     private Boolean isLastPage;
+    private String loginProfileImgUrl;
     private List<FeedReviewResponseDto> reviewList;
 
     @Builder
-    private FeedPageResponseDto(Boolean isLastPage, List<FeedReviewResponseDto> reviewList) {
+    private FeedPageResponseDto(Boolean isLastPage, String loginProfileImgUrl, List<FeedReviewResponseDto> reviewList) {
         this.isLastPage = isLastPage;
+        this.loginProfileImgUrl = loginProfileImgUrl;
         this.reviewList = reviewList;
     }
 
-    public static FeedPageResponseDto of(Boolean isLastPage, List<FeedReviewResponseDto> reviewList) {
+    public static FeedPageResponseDto of(Boolean isLastPage, String loginProfileImgUrl, List<FeedReviewResponseDto> reviewList) {
         return FeedPageResponseDto.builder()
                 .isLastPage(isLastPage)
+                .loginProfileImgUrl(loginProfileImgUrl)
                 .reviewList(reviewList)
                 .build();
     }
